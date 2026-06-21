634a905b510f7433e86a896e67ab02b60edb26c3
Big-Sheriff <psugak@o2.pl>
2026-06-21 14:25:15 +0100

    add game clock and delta time

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,7 +19,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.update()
-
+        dt = clock.tick(60) / 1000.0  # Limit to 60 FPS and get delta time in seconds
 
 if __name__ == "__main__":
     main()
